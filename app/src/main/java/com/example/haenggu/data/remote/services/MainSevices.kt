cbd7be6ee16f011744723085a1a7c9eebdc13ee5
gut27 <gracious7272@gmail.com>
2022-01-19 23:52:18 +0900

    V1

--- app/src/main/java/com/example/haenggu/data/remote/services/MainSevices.kt
+++
@@ -1,4 +0,0 @@
-package com.example.haenggu.data.remote.services
-
-class MainSevices {
-}
--- app/src/main/java/com/example/haenggu/join/JoinFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.haenggu.join
-
-//import androidx.fragment.app.Fragment
-//
-//class JoinFragment : Fragment {
-//}
