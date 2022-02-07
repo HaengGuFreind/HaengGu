507faa6dfbb733d9081ed4c337da7a8dbeb96c71
Yunhyunjo <gotqlc147@likelion.org>
2022-02-07 23:11:26 +0900

    mainView setting2

--- app/src/main/java/com/example/haenggu/main/MainActivity.kt
+++ app/src/main/java/com/example/haenggu/main/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.haenggu.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.haenggu.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //activity_main_vp.adapter = MainPagerAdapter(supportFragmentManager)
-        //activity_main_vp.offscreenPageLimit = 2;
+        activity_main_vp.adapter = MainPagerAdapter(supportFragmentManager)
+        activity_main_vp.offscreenPageLimit = 2
     }
 }
