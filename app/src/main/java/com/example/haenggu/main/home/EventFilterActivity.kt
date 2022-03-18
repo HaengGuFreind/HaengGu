66a0576060323d24ccf8c03ea303647abc626ff1
Yunhyunjo <gotqlc147@likelion.org>
2022-03-18 09:42:35 +0900

    EventFilter Spinner

--- app/src/main/java/com/example/haenggu/main/home/EventFilterActivity.kt
+++ app/src/main/java/com/example/haenggu/main/home/EventFilterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.haenggu.main.home
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -12,6 +13,7 @@ class EventFilterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEventFilterBinding
     private val item = listOf("동아리행사", "박람회", "컨퍼런스", "연극/뮤지컬", "전시", "페스티벌", "콘서트", "기타")
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEventFilterBinding.inflate(layoutInflater)
@@ -29,22 +31,23 @@ class EventFilterActivity : AppCompatActivity() {
         val spinner1 = binding.spTag1
         val spinner2 = binding.spTag2
         val spinner3 = binding.spTag3
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, item)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.tags, R.layout.item_spinner)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, R.layout.item_spinner, item)
         spinner1.adapter = adapter
         spinner2.adapter = adapter
         spinner3.adapter = adapter
 
 
         binding.ivClose.setOnClickListener{
-            finish()
             overridePendingTransition(R.anim.none, R.anim.vertical_exit)
+            finish()
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home ->{
-
             }
         }
         return super.onOptionsItemSelected(item)
