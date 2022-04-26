f534f2ceada1945f9860dfedd16525673cfe30a7
Yunhyunjo <gotqlc147@likelion.org>
2022-04-26 16:55:51 +0900

    EventBorad UI

--- app/src/main/java/com/example/haenggu/data/remote/datasources/ResponseEventDetail.kt
+++ app/src/main/java/com/example/haenggu/data/remote/datasources/ResponseEventDetail.kt
@@ -1,6 +1,7 @@
 package com.example.haenggu.data.remote.datasources
 
 data class ResponseEventDetail (
+    val boards: List<Boards>,
     val event_id: String,
     val title: String,
     val description: String,
@@ -15,4 +16,10 @@ data class ResponseEventDetail (
     val region: String,
     val tag: List<String>,
     val image_url: List<String>,
+)
+
+data class Boards(
+    val board_id: String,
+    val title: String,
+    val user: BoardUser,
 )
